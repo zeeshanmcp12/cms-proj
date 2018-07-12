7a28dc333117840504f5851ac16e28a37fbd6f1e
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2018-07-12 12:36:22 +0500

    included function code in side_nav file

--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -2,23 +2,24 @@
 <p class="lead">Cake Ordering System</p>
 <div class="list-group">
 
-    <?php
-        $query = "SELECT * FROM categories " ;
-        $send_query = mysqli_query($connection, $query);
-
-        if(!$send_query){
-
-            die("QUERY FAILED" . mysqli_error($connection));
-        }
-
-        while($row = mysqli_fetch_array($send_query)){      //categories ko database se dynamically fetch karne k liye while ka loop lagaya hai
-
-            echo "<a href='' class='list-group-item'>{$row['cat_title']}</a>"; //square bracket main agar cat_title nahi likhenge tu categories table k cat_title se data fetch nahi hoga.
-        }
+    <?php get_categories(); ?>
+    
+<!--
+//        $query = "SELECT * FROM categories " ;
+//        $send_query = mysqli_query($connection, $query);
+//
+//        if(!$send_query){
+//
+//            die("QUERY FAILED" . mysqli_error($connection));
+//        }
+//
+//        while($row = mysqli_fetch_array($send_query)){      //categories ko database se dynamically fetch karne k liye while ka loop lagaya hai
+//
+//            echo "<a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>"; //square bracket main agar cat_title nahi likhenge tu categories table k cat_title se data fetch nahi hoga.
+//        }
    
-    ?>
 
-<!-- <a href="category.html" class="list-group-item">Wedding Cake</a>
+<a href="category.html" class="list-group-item">Wedding Cake</a>
 <a href="#" class="list-group-item">Birthday Cake</a>
 <a href="#" class="list-group-item">Aniversary Cake</a> -->
 
