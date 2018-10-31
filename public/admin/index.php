8f7b8cbc255f4e576f4858605ebfac615d5f343d
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2018-10-31 19:25:50 +0500

    initial setup done on admin panel

--- public/admin/index.php
+++ public/admin/index.php
@@ -24,7 +24,31 @@
             if ($_SERVER['REQUEST_URI'] == "/cms-proj/public/admin/" || $_SERVER['REQUEST_URI'] == "/cms-proj/public/admin/index.php"){
 
                 include(TEMPLATE_BACK . "/admin_content.php");
-            } 
+            }
+
+            if (isset($_GET['orders'])){
+
+                include(TEMPLATE_BACK . "/orders.php");
+
+            }
+
+            if (isset($_GET['categories'])){
+
+                include(TEMPLATE_BACK . "/categories.php");
+
+            }
+
+            if (isset($_GET['products'])){
+
+                include(TEMPLATE_BACK . "/products.php");
+
+            }
+
+            if (isset($_GET['add_product'])){
+
+                include(TEMPLATE_BACK . "/add_product.php");
+
+            }
         
         ?>
 
