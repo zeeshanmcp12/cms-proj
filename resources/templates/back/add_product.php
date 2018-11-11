955fb41a06774d4cef92f518003d50cc57cc0c62
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2018-11-11 17:16:59 +0500

    fields working complete in addProduct page

--- resources/templates/back/add_product.php
+++ resources/templates/back/add_product.php
@@ -41,6 +41,12 @@
         <input type="number" name="product_price" class="form-control" size="60">
       </div>
     </div>
+
+
+    <div class="form-group">
+           <label for="product-title">Product Short Description</label>
+      <textarea name="product_description" id="" cols="30" rows="3" class="form-control"></textarea>
+    </div>
   
 
 </div><!--Main Content-->
@@ -62,7 +68,6 @@
 
     <div class="form-group">
          <label for="product-title">Product Category</label>
-          <hr>
         <select name="product_category" id="" class="form-control">
             <option value="">Select Category</option>
            
@@ -76,21 +81,19 @@
 
 
     <div class="form-group">
-      <label for="product-title">Product Brand</label>
-         <select name="product_brand" id="" class="form-control">
-            <option value="">Select Brand</option>
-         </select>
+      <label for="product-title">Product Quantity</label>
+         <input type="number" class="form-control">
     </div>
 
 
 <!-- Product Tags -->
 
 
-    <div class="form-group">
+    <!-- <div class="form-group">
           <label for="product-title">Product Keywords</label>
           <hr>
         <input type="text" name="product_tags" class="form-control">
-    </div>
+    </div> -->
 
     <!-- Product Image -->
     <div class="form-group">
