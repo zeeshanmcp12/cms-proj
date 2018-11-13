80928dcd0cbe59d62e44ce7a2d3c83f3eba5bf90
zeeshanmcp12 <zeeshanmcp12@gmail.com>
2018-11-13 08:58:53 +0500

    edit product bug fix

--- public/admin/index.php
+++ public/admin/index.php
@@ -43,6 +43,11 @@ if(!isset($_SESSION['username'])){
                 include(TEMPLATE_BACK . "/add_product.php");
 
             }
+            if (isset($_GET['edit_product'])){
+
+                include(TEMPLATE_BACK . "/edit_product.php");
+
+            }
 
             if (isset($_GET['users'])){
 
